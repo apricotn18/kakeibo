60644a851e91daeee7af948c9b906c809b034059
kyoko nozaki <nozaki_kyoko@waku-2.com>
2023-11-16 20:07:35 +0900

    complete

--- pages/component/summary/Summary.tsx
+++ pages/component/summary/Summary.tsx
@@ -4,8 +4,14 @@ import style from './summary.module.scss';
 export default function Index() {
 	return (
 		<div className={style.summary}>
-			合計：10000円<br />
-			<span>きょうこ</span>から〇〇へ<span>〇〇円</span>払う
+			<div className={style.summary_inner}>
+				<p className={style.summary_total}>
+					合計<span className={style.summary_totalNum}>10,000円</span>
+				</p>
+				<p className={style.summary_label}>
+					<span>きょうこ</span>が<span>ゆうじ</span>へ<span>2,000円払う</span>
+				</p>
+			</div>
 		</div>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './component/header/Header';
 import Summary from './component/summary/Summary';
+import Tab from './component/tab/Tab';
 
 export default function Index() {
 	return (
 		<>
 			<Header />
 			<Summary />
-			<ul>
-				<li>ゆうじに追加</li>
-				<li>きょうこ</li>
-			</ul>
+			<Tab />
 		</>
 	);
 }
